b1f1d8f5b326f66b692e9046a1ef9eafab622f92
Ahmed Alsardi <ahmed.alsardi@gmail.com>
2023-08-05 13:45:16 +0300

    adding python configuration

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,6 @@
 ---@type ChadrcConfig 
 local M = {}
 M.ui = {theme = 'kanagawa'}
--- M.plugins = require "custom.plugins"
+M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
